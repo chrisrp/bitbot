e78fd16204baa79cb28be0d7c432230962247729
Christiano Rodrigues <chris@Christianos-Air.fritz.box>
2018-07-24 20:21:01 +0200

    adding failing test

--- spec/models/order_config_spec.rb
+++ spec/models/order_config_spec.rb
@@ -2,7 +2,7 @@ require 'rails_helper'
 
 RSpec.describe OrderConfig do
   it 'saves config' do
-    FactoryBot.create(:order_config)
+#    FactoryBot.create(:order_config)
     expect(OrderConfig.count).to eq(1)
   end
 end
